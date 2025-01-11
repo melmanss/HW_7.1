edef10cc96513c74040bb1b12792807bb6dfca62
melmanss <74724829+melmanss@users.noreply.github.com>
2025-01-11 15:50:48 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] array = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(201) - 100;
+        }
+        System.out.println("Створений масив: " + Arrays.toString(array));
+        int negativeSum = 0;
+        for (int num : array) {
+            if (num < 0) {
+                negativeSum += num;
+            }
+        }
+        System.out.println("Сума від'ємних чисел: " + negativeSum);
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int num : array) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        System.out.println("Кількість парних чисел: " + evenCount);
+        System.out.println("Кількість непарних чисел: " + oddCount);
+        int max = array[0];
+        int min = array[0];
+        int maxIndex = 0;
+        int minIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+            }
+            if (array[i] < min) {
+                min = array[i];
+                minIndex = i;
+            }
+        }
+        System.out.println("Найбільший елемент: " + max + ", індекс: " + maxIndex);
+        System.out.println("Найменший елемент: " + min + ", індекс: " + minIndex);
+        int firstNegativeIndex = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                firstNegativeIndex = i;
+                break;
+            }
+        }
+        if (firstNegativeIndex != -1) {
+            int sumAfterNegative = 0;
+            int countAfterNegative = 0;
+            for (int i = firstNegativeIndex + 1; i < array.length; i++) {
+                sumAfterNegative += array[i];
+                countAfterNegative++;
+            }
+            if (countAfterNegative > 0) {
+                double averageAfterNegative = (double) sumAfterNegative / countAfterNegative;
+                System.out.println("Середнє арифметичне після першого від'ємного числа: " + averageAfterNegative);
+            } else {
+                System.out.println("Після першого від'ємного числа немає інших елементів.");
+            }
+        } else {
+            System.out.println("У масиві немає від'ємних чисел.");
         }
     }
 }
